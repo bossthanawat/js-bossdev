55c2e51724af5d3354494ed223784895fb58593c
bossthanawat <boss41680@gmail.com>
2024-02-20 18:23:03 +0700

    Refactor LinkTypography and IconHambuger components

--- apps/next-portfolio/app/components/layout/LayoutNavbar.tsx
+++ apps/next-portfolio/app/components/layout/LayoutNavbar.tsx
@@ -127,8 +127,14 @@ type LinkTypographyProps = {
   isCurrentPath?: boolean;
   children: React.ReactNode;
 } & LinkProps;
-const LinkTypography = (props: LinkTypographyProps) => {
-  const { newTab, styleHighlight, children, href, isCurrentPath } = props;
+const LinkTypography = ({
+  isCurrentPath,
+  styleHighlight,
+  newTab,
+  children,
+  href,
+  ...props
+}: LinkTypographyProps) => {
   return (
     <Link
       {...props}
@@ -198,7 +204,7 @@ const IconHambuger = () => {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 24 24"
       height="1em"
       width="1em"
