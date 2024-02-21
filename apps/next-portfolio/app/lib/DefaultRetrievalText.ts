878d2229b1337dd9998fcce4fd7a04c99ecfe0de
bossthanawat <boss41680@gmail.com>
2024-02-21 13:50:10 +0700

    Fix formatting and add project details

--- apps/next-portfolio/app/lib/DefaultRetrievalText.ts
+++ apps/next-portfolio/app/lib/DefaultRetrievalText.ts
@@ -1,4 +1,4 @@
-export const DefaultRetrievalText =  `# Hi I'm Boss Thanawat
+export const DefaultRetrievalText = `# Hi I'm Boss Thanawat
 Software Engineer and Front-End Developer
 Hi, my name is name is Thanawat Kittichaikarn.
 Experienced front-end developer and software engineer with a focus on creating accessible web products. Proficient in React and dedicated to crafting visually appealing and user-friendly interfaces.
@@ -35,27 +35,33 @@ Where I’ve Worked
 Easy Rice Digital Technology
 Front-End Developer
 Feb 2021 - Present
--   Write modern, performant, maintainable code for a diverse array of client and internal projects
--   Lead the development of accessible web products
--   Collaborated with cross-functional teams to deliver high-quality and user-centric solutions.
+- Write modern, performant, maintainable code for a diverse array of client and internal projects
+- Lead the development of accessible web products
+- Collaborated with cross-functional teams to deliver high-quality and user-centric solutions.
+- Projects
+	- M0 Web application AI Solution for Rice Quality Inspection
+	- MP Web application AI Solution for Paddy Varieties Inspection
+	- Company website Easyrice and Exambrisk
+	- Khakhao Web application and LIFF (Line) rice trading 
+	- Easyrice M Portal including many products (MI, Coffee, etc.)
 
 NTT Global
 Software Engineer
 Nov 2019 - Jan 2021
--   Developed a web application for managing the production process, catering specifically to the needs of Tesco.
--   Contributed to the design and implementation of interfaces.
--   Utilized Robotic Process Automation (RPA) to enhance and streamline services for clients
+- Developed a web application for managing the production process, catering specifically to the needs of Tesco.
+- Contributed to the design and implementation of interfaces.
+- Utilized Robotic Process Automation (RPA) to enhance and streamline services for clients
 
 Dimension Data
 Software Engineer
 Jun 2017 - Oct 2019
--   PDF generation for the fax system used by banks
--   implemented Interactive Voice Response (IVR) systems, managing various flow logics for user interactions, notably for clients such as Agoda.
--   Oversaw and maintained diverse systems based on client products, ensuring seamless functionality.
+- PDF generation for the fax system used by banks
+- implemented Interactive Voice Response (IVR) systems, managing various flow logics for user interactions, notably for clients such as Agoda.
+- Oversaw and maintained diverse systems based on client products, ensuring seamless functionality.
 
 Education
 King Mongkut's Institute of Technology Ladkrabang
-Bachelor's Degree in Computer Engineering (Information)
+Bachelor's Degree in Computer Engineering Department, majoring in Information Engineering.
 2013 - 2016`;
 
 export default DefaultRetrievalText;
