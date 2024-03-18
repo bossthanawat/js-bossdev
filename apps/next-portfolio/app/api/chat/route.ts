e658a99234ba2b2e8266b7fad97d90ed78f390c2
bossthanawat <boss41680@gmail.com>
2024-03-18 11:01:44 +0700

    Add dependencies and update chat route

--- apps/next-portfolio/app/api/chat/route.ts
+++ apps/next-portfolio/app/api/chat/route.ts
@@ -1,7 +1,4 @@
-import { OpenAI } from 'langchain/llms/openai';
-import { loadQAStuffChain } from 'langchain/chains';
 import { Document } from 'langchain/document';
-import DefaultRetrievalText from '../../lib/DefaultRetrievalText';
 import { NextRequest, NextResponse } from 'next/server';
 import { createStuffDocumentsChain } from 'langchain/chains/combine_documents';
 
@@ -13,52 +10,75 @@ import {
 } from '@langchain/core/prompts';
 import { ChatMessageHistory } from 'langchain/stores/message/in_memory';
 
+import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
+import { HarmBlockThreshold, HarmCategory } from '@google/generative-ai';
+import DefaultRetrievalText from '../../lib/DefaultRetrievalText';
+
+export type UnitType = "human" | "ai"
+
+type ValueChat = {
+  role: UnitType
+  content: string;
+  isLoading?: boolean;
+}
+
 export async function POST(request: NextRequest) {
   try {
-    const { messages } = await request.json();
-    const demoEphemeralChatMessageHistory = new ChatMessageHistory();
-    const docs = [new Document({ pageContent: DefaultRetrievalText })];
-    const chat = new ChatOpenAI({
-      modelName: 'gpt-3.5-turbo-1106',
-      temperature: 0.6,
-      openAIApiKey: process.env.OPENAI_API_KEY,
+    const body = await request.json();
+    const messages = body.messages as ValueChat[];
+
+    const chat = new ChatGoogleGenerativeAI({
+      apiKey: process.env.GOOGLE_API_KEY,
+      modelName: 'gemini-pro',
+      maxOutputTokens: 2048,
+      temperature: 0.4,
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        },
+      ],
     });
 
+    //Prompt Templates
     const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
       [
         'system',
-        "You are a helpful assistant. Answer all questions to the best of your ability based on the below context:{context}",
+        'You are a helpful assistant. I have additional information to context:{context}. You can answer things outside of context. ',
       ],
       new MessagesPlaceholder('messages'),
     ]);
+
+    // Chat History
+    const ephemeralChatMessageHistory = new ChatMessageHistory();
+    for (const message of messages) {
+      if (message.role === 'human') {
+        await ephemeralChatMessageHistory.addMessage(
+          new HumanMessage(message.content)
+        );
+      }
+      if (message.role === 'ai') {
+        await ephemeralChatMessageHistory.addMessage(
+          new AIMessage(message.content)
+        );
+      }
+    }
+
+    // Retrievers
+    const docs = [new Document({ pageContent: DefaultRetrievalText })];
 
     const documentChain = await createStuffDocumentsChain({
       llm: chat,
       prompt: questionAnsweringPrompt,
     });
 
-    for (const message of messages) {
-      if (message.human) {
-        await demoEphemeralChatMessageHistory.addMessage(
-          new HumanMessage(message.human)
-        );
-      }
-      if (message.ai) {
-        await demoEphemeralChatMessageHistory.addMessage(
-          new AIMessage(message.ai)
-        );
-      }
-    }
-
-    const messageHistory = await demoEphemeralChatMessageHistory.getMessages();
-
     const responseMessage = await documentChain.invoke({
-      messages: messageHistory,
+      messages: await ephemeralChatMessageHistory.getMessages(),
       context: docs,
     });
 
     return NextResponse.json({
-      text: responseMessage,
+      content: responseMessage,
     });
   } catch (e) {
     console.log('err', e);
--- apps/next-portfolio/app/components/sections/home/Chat.tsx
+++ apps/next-portfolio/app/components/sections/home/Chat.tsx
@@ -1,192 +1,86 @@
 'use client';
 
-import { Input, Skeleton } from '@js-bossdev/components';
-import { Controller, useForm } from 'react-hook-form';
-import { FormProvider } from '../../hook-form';
-import React, { useEffect, useRef, useState } from 'react';
-import Image from 'next/image';
+import React, { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
-import Scrollbar from '../../Scrollbar';
-import BouncingDotsLoader from '../../BouncingDotsLoader';
+import Chat, { ValueChat, ValueForm } from '../../Chat';
+import { UnitType } from '../../../api/chat/route';
 
-// import { ChatMessageHistory } from 'langchain/stores/message/in_memory';
-// import { HumanMessage, AIMessage } from '@langchain/core/messages';
-
-type ValueForm = {
-  message: string;
-};
-
-type StateChat = {
-  id: string;
-  send: string;
-  result?: string;
-  isLoading?: boolean;
-};
-
-const Chat = () => {
-  // const [demoEphemeralChatMessageHistory] = useState(new ChatMessageHistory());
-
-  const endRef = useRef<HTMLDivElement>(null);
+const ChatSection = () => {
   const mutateGPT = useMutation({
     mutationFn: ({ messages }: { messages: any }) =>
-      axios.post('/api/chat', {
-        messages: messages,
-      }, {
-        timeout: 30*1000,
-      }),
+      axios.post(
+        '/api/chat',
+        {
+          messages: messages,
+        },
+        {
+          timeout: 30 * 1000,
+        }
+      ),
   });
 
-  const [chats, setChats] = useState<StateChat[]>([]);
-  const methods = useForm<ValueForm>({
-    defaultValues: {
-      message: '',
-    },
-  });
-  const { control, handleSubmit, reset } = methods;
+  const [chats, setChats] = useState<ValueChat[]>([]);
+
   const onSubmit = async (data: ValueForm) => {
-    endRef.current?.scrollIntoView({ behavior: 'smooth' });
-    const id = uuidv4();
     if (data.message === '') return;
-    reset();
-    const newChat = [...chats, { id, send: data.message, isLoading: true }];
+    const humanMessage = {
+      content: data.message,
+      role: 'human' as UnitType,
+    };
+
     setChats((prev) => [
       ...prev,
+      humanMessage,
       {
-        id,
-        send: data.message,
+        content: '',
+        role: 'ai' as UnitType,
         isLoading: true,
       },
     ]);
 
-    const getMessages = newChat.map((e) => {
-      return {
-        human: e.send,
-        ai: e.result,
-      };
-    });
-
     mutateGPT.mutate(
-      { messages: getMessages },
+      { messages: [...chats, humanMessage] },
       {
         onSuccess: async ({ data }) => {
-          setChats((prev) =>
-            prev.map((chat) =>
-              chat.id === id
-                ? { ...chat, result: data.text, isLoading: false }
-                : chat
-            )
-          );
+          setChats((prev) => {
+            const items = prev.slice(0, prev.length - 1);
+            return [
+              ...items,
+              {
+                content: data.content,
+                role: 'ai' as UnitType,
+                isLoading: false,
+              },
+            ];
+          });
         },
         onError: (error) => {
-          setChats((prev) =>
-            prev.map((chat) =>
-              chat.id === id
-                ? {
-                    ...chat,
-                    result: 'Something went wrong :(',
-                    isLoading: false,
-                  }
-                : chat
-            )
-          );
+          setChats((prev) => {
+            const items = prev.slice(0, prev.length - 1);
+            return [
+              ...items,
+              {
+                content: 'Something went wrong :(',
+                role: 'ai' as UnitType,
+                isLoading: false,
+              },
+            ];
+          });
         },
       }
     );
   };
 
-  useEffect(() => {
-    endRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [chats]);
-
   return (
     <>
-      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-4 p-4 ">
-          {chats.length > 0 ? (
-            <Scrollbar className="h-80 overflow-y-auto p-2">
-              <div className="flex flex-col gap-2">
-                {chats.map((chat, index) => (
-                  <React.Fragment key={index}>
-                    <div className="flex gap-2 items-center">
-                      <Image
-                        src={'/assets/images/notion-avatar-1.png'}
-                        alt="user"
-                        width={50}
-                        height={50}
-                      />
-                      <div className="bg-gray-100 p-3 rounded-r-xl rounded-bl-xl">
-                        <p className="text-sm">{chat.send}</p>
-                      </div>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                      <Image
-                        src={'/assets/images/notion-avatar-bot.png'}
-                        alt="bot"
-                        width={50}
-                        height={50}
-                      />
-                      {chat?.isLoading ? (
-                        <BouncingDotsLoader />
-                      ) : (
-                        <div className="bg-orange-100 p-3 rounded-r-xl rounded-bl-xl">
-                          <p className="text-sm">{chat?.result}</p>
-                        </div>
-                      )}
-                    </div>
-                  </React.Fragment>
-                ))}
-                <div ref={endRef}></div>
-              </div>
-            </Scrollbar>
-          ) : (
-            <>
-              <div className="flex gap-2 items-center">
-                <Image
-                  src={'/assets/images/notion-avatar-bot.png'}
-                  alt="user"
-                  width={50}
-                  height={50}
-                />
-                <div className="bg-gray-100 p-3 rounded-r-xl rounded-bl-xl">
-                  <p className="text-sm">
-                    Please ask me something. (づ ◕‿◕ )づ
-                  </p>
-                </div>
-              </div>
-            </>
-          )}
-          <Controller
-            name={'message'}
-            control={control}
-            render={({ field }) => (
-              <div className="relative">
-                <Input
-                  placeholder="Type your message"
-                  className=" pr-8"
-                  {...field}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                  className="absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer"
-                  onClick={handleSubmit(onSubmit)}
-                >
-                  <path
-                    fill="currentColor"
-                    d="M13 20h-2V8l-5.5 5.5l-1.42-1.42L12 4.16l7.92 7.92l-1.42 1.42L13 8z"
-                  ></path>
-                </svg>
-              </div>
-            )}
-          />
-        </div>
-      </FormProvider>
+      <Chat
+        chats={chats}
+        onSubmit={onSubmit}
+        disabledType={mutateGPT.isPending}
+      />
     </>
   );
 };
 
-export default Chat;
+export default ChatSection;
