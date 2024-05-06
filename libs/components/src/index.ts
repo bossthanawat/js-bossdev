e1de8a20f936b1883f7ea2717e8ebe1a4f8e1d28
bossthanawat <boss41680@gmail.com>
2024-05-06 20:19:14 +0700

    Add Separator component to the components library

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/badge';
 export * from './lib/drawer';
 export * from './lib/tooltip';
 export * from './lib/input';
+export * from './lib/separator';
 export * from './lib/skeleton';
 export * from './lib/sonner';
 
